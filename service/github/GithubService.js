6cb81ff431a4c3c045f8d2b428bbc0b90bd0259f
KazuwoKiwame12 <39262724+KazuwoKiwame12@users.noreply.github.com>
2021-05-07 18:04:09 +0900

    implement the function

--- service/github/GithubService.js
+++ service/github/GithubService.js
@@ -16,4 +16,42 @@ export class GithubService {
     })
     return result
   }
+
+  async getProjectsInRepository({owner, repo}) {
+    const result = await this._oktokit.rest.projects.listForRepo({
+      owner,
+      repo
+    })
+    const projects_info = result.map(x => {
+      return {
+        project_id: x.id,
+        name: x.name
+    }})
+    return projects_info
+  }
+
+  async getProjectColumns({project_id}) {
+    const result = await this._octokit.rest.projects.listColumns({
+      project_id
+    });
+    const columns_info = result.map(x => {
+      return {
+        column_id: x.id,
+        name: x.name
+      }})
+    return columns_info
+  }
+
+  async addIssuesToProject({column_id, note, issues}) {//issues...[{id int, type string}]
+    // TODO issuesのオブジェクトを宣言して、instanceofでバリデーションするかの確認
+    for(const {id, type} of issues) {
+      // TODO エラー判定いるかの確認
+      await this._octokit.rest.projects.createCard({
+        column_id,
+        note,
+        id,
+        type,
+      });
+    }
+  }
 }
